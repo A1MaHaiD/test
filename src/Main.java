61e97c5296aa706cf1a6485eb42dc734d26025a7
A1MaHaiD <super.lukianchuk2010@gmail.com>
2023-12-04 13:47:53 +0200

    add README.MD

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+/*
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "initial commit"
+git branch -M main
+git remote add origin git@github.com:A1MaHaiD/test.git
+git push -u origin main
+*/
+
+/*
+ssh-keygen -t ed25519 -C "super.lukianchuk2010@gmail.com"
+cat ~/.ssh/id_ed25519.pub | clip
+*/
